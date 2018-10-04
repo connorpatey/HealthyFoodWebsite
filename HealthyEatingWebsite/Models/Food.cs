7a6bcec70f62d17e1a5f920c38760995806e097d
21701762 <21701762@bucks.ac.uk>
2018-10-04 13:10:00 +0100

    Modified site master

---
+++ HealthyEatingWebsite/Models/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthyEatingWebsite.Models
+{
+    public class Food
+    {
+        public int FoodID { get; set; }
+
+        public string Brand { get; set; }
+    }
+}
